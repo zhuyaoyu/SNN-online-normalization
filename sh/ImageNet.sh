0cf12aec10b9af63f3ed517b8667fc6f8b9e9084
zhuyaoyu <1901112058@pku.edu.cn>
2023-08-27 12:59:53 +0800

    update config

--- sh/ImageNet.sh
+++ sh/ImageNet.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port 29000 train.py -config yamls/ImageNet.yaml;
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port 29000 train.py -config yamls/ImageNet_base1.yaml;
 shutdown -h now;
